fa5aef250776f80e7b24c3a1472e062f2efc133e
Tommy Connor <tconnor98@aol.com>
2019-10-28 21:44:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,11 +20,11 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees).each do |badge|
-    
+  batch_badge_creator(attendees).each do |badge|
+    puts "#{badge}"
   end
-  puts assign_rooms(attendees).each do |assignment|
-    
+  assign_rooms(attendees).each do |assignment|
+    puts "#{assignment}"
   end
 end
   
