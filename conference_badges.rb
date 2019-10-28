c27669f47c9372034f3586a24b92c7fcb1bef08e
Tommy Connor <tconnor98@aol.com>
2019-10-28 20:25:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |attendee|
+    badges << "Hello, my name is #{attendee}."
+  end
+  return badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index do |attendee, index|
+    room_assignments << "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+  end
+  return room_assignments
+end
+
+def printer()
+  badges_and_room_assignments = []
+  badges_and_room_assignments << batch_badge_creator(attendees)
+  badges_and_room_assignments << assign_rooms(attendees)
+  puts badges_and_room_assignments.chomp
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
